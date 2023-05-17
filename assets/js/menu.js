f747af28410fafc91554529c23a6d64ac0cb98ed
Mohand Arezki lahlou <91838551+MohandArezki@users.noreply.github.com>
2023-05-17 16:25:07 +0100

    Delete assets directory

--- assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-// Hamburger menu
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav_links");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  hamburger.classList.toggle("active");
-});
